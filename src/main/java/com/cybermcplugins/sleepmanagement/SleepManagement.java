4ba48cde1dfc749871a97b01347b678f287d9a3f
BuildTools <jlake624@gmail.com>
2023-10-20 11:06:05 -0400

    Updated for 1.20.2. Got the plugin to full workability.

--- src/main/java/com/cybermcplugins/sleepmanagement/SleepManagement.java
+++ src/main/java/com/cybermcplugins/sleepmanagement/SleepManagement.java
@@ -5,17 +5,15 @@ import com.cybermcplugins.sleepmanagement.listeners.GetsUp;
 import com.cybermcplugins.sleepmanagement.listeners.LaysDown;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
 
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.UUID;
 
 public final class SleepManagement extends JavaPlugin {
 
     private ArrayList<UUID> wantsDay = new ArrayList<>();
-    private BukkitTask clearWantsDay;
-    private long clearWantsDayDelay;
 
 
     @Override
@@ -23,29 +21,29 @@ public final class SleepManagement extends JavaPlugin {
 
         this.getConfig().options().copyDefaults();
         saveDefaultConfig();
-        clearWantsDayDelay = this.getConfig().getLong("delay");
 
         Bukkit.getPluginManager().registerEvents(new LaysDown(this, this.getConfig().getBoolean("usePercent")), this);
         Bukkit.getPluginManager().registerEvents(new GetsUp(this), this);
-        getCommand("sleep").setExecutor(new Sleep(this, this.getConfig().getBoolean("usePercent")));
+        Objects.requireNonNull(getCommand("sleep")).setExecutor(new Sleep(this));
+        this.getLogger().info("Percent of players required: " + getPercentOfPlayers());
 
     }
 
     public ArrayList<UUID> getWantsDay(){return wantsDay;}
 
-    @SuppressWarnings( {"deprecation"} )
     public void clearWantsDay() {
-        this.clearWantsDay = Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
-            for (UUID a: wantsDay) {
-                wantsDay.remove(a);
-            }
-        });
+        this.getLogger().info("Before: " + wantsDay.toString());
+        wantsDay = new ArrayList<>();
+
+        this.getLogger().info("After: " + wantsDay.toString());
+    }
+    public double getPercentOfPlayers() {
+        if(this.getConfig().getDouble("percent") == 0){
+            return 100;
+        }else{
+            return this.getConfig().getDouble("percent");
+        }
     }
 
-    public boolean isUsePercent() { return this.getConfig().getBoolean("usePercent");}
-
-    public int getPercentOfPlayers() {return this.getConfig().getInt("percent");}
-
-    public int getAmountOfPlayers() {return this.getConfig().getInt("amount"); }
 
 }
--- src/main/java/com/cybermcplugins/sleepmanagement/commands/Sleep.java
+++ src/main/java/com/cybermcplugins/sleepmanagement/commands/Sleep.java
@@ -11,12 +11,10 @@ import org.bukkit.entity.Player;
 
 public class Sleep implements CommandExecutor {
 
-    private SleepManagement sleep;
-    private boolean usePercent;
+    private final SleepManagement sleep;
 
-    public Sleep(SleepManagement sleep, boolean usePercent){
+    public Sleep(SleepManagement sleep){
         this.sleep = sleep;
-        this.usePercent = usePercent;
     }
 
     @Override
@@ -25,19 +23,21 @@ public class Sleep implements CommandExecutor {
         if(sender instanceof Player) {
             Player player = (Player) sender;
             World world = player.getWorld();
-            sleep.getWantsDay().add(player.getUniqueId());
+            if(!sleep.getWantsDay().contains(player.getUniqueId()))
+                sleep.getWantsDay().add(player.getUniqueId());
             if (world.getTime() >= 12769) {
-                if (sleep.getWantsDay().size() >= 1) {
+                if (((double) sleep.getWantsDay().size() / Bukkit.getOnlinePlayers().size())*100 >= sleep.getPercentOfPlayers()) {
                     world.setTime(6000);
                     sleep.clearWantsDay();
                 } else
                     Bukkit.broadcastMessage(ChatColor.BOLD.GRAY + "[" + ChatColor.GREEN + "SleepManagement" + ChatColor.BOLD.GRAY + "] "
-                            + ChatColor.BOLD + player.getDisplayName() + " wants day. Either two people find a bed or" +
-                            " one person can sleep and then another can do /sleep");
+                            + ChatColor.BOLD + player.getDisplayName() + " wants day.\n" + "Current players who want day: " +
+                            (((double) sleep.getWantsDay().size() / Bukkit.getOnlinePlayers().size())*100) + "\nRequired percent of players: " +
+                            sleep.getPercentOfPlayers() + "\nIf you wish for it to be day time either find a bed or do /sleep");
             } else
                 player.sendMessage(ChatColor.RED + "It is not nighttime!");
         }else
-            sender.sendMessage(ChatColor.BOLD.RED + "Just use /time set day");
+            sender.sendMessage(ChatColor.BOLD.RED + "Just use time set day");
 
         return false;
     }
